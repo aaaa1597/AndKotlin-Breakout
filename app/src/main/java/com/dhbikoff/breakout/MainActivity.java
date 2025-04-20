f01e59c3e40607ccb7a1e3234207f3f030e54e14
aaaa1597 <tokushi2011@gmail.com>
2025-04-20 16:51:14 +0900

    UI作成中

--- app/src/main/java/com/dhbikoff/breakout/MainActivity.java
+++ app/src/main/java/com/dhbikoff/breakout/MainActivity.java
@@ -14,11 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
